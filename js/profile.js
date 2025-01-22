6d251a9d3d7563335250c69f855aaf7870030193
Nicoaude <nicolasauderut@gmail.com>
2025-01-21 21:17:33 -0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -9,7 +9,7 @@ if (user) {
     const profileBox = document.createElement("div")
     profileBox.classList.add("profile__box")
     profileBox.innerHTML = `
-        <img src="/img/profile.jpg" alt="Profile picture">
+        <img src="../img/profile.jpg" alt="Profile picture">
         <h1>${user.name} ${user.lastname}</h1>
         <h3>ID:</h3> <p>${user.id}</p>
         <h3>EDAD:</h3> <p>${user.age}</p>
